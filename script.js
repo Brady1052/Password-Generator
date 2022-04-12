214e81f60e8b91810e296764db05bee3e3a539eb
Brady Buchholz <bradybuchholz@gmail.com>
2022-04-12 14:06:46 -0400

    function for generating a special character created

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+const res = require("express/lib/response");
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+//Creates empty array for pushing values to
 let password = [];
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -20,7 +24,7 @@ function generatePassword() {
     var numbers = confirm("Do you want to include numbers in your password");
     console.log(numbers)
     var special = confirm("Do you want to include special characters in your password?");
-  
+    console.log(special)
     var upperCases = confirm("Do you want to included upper case letters in your password?");
   
     var lowerCases = confirm("Do you want to include lower case letters in your password?");
@@ -33,38 +37,42 @@ function generatePassword() {
       var number = Math.floor((Math.random() * 10) + 1);
       console.log(number)
       password.push(number)
-      console.log('Passowrd: ' + number)
+      console.log('Password: ' + number)
     }
     //If the user selected yes to special characters then generate a random special character
-    var specialCh = '!@#$%^&*'
    if (special){
-   var random = Math.floor((Math.random() * 10) + 1)
-   } if (random == '1'){
-   console.log('!')
-   } else if (random == '2'){
-     console.log('@')
-   }else if (random == '3') {
-     console.log('#')
-   } else if (random == '4') {
-     console.log('$')
-   }else if (random == '5') {
-     console.log('%')
-   }else if (random =='6'){
-   console.log('^')
-} else if (random == '7'){
-  console.log('&')
-} else if (random == '8'){
-  console.log('*')
-} else if (random == '9'){
-  console.log('(')
-}else if (random == '10') {
-  console.log(')')
-} else {
-  console.log('$')
-}
-}
+   specialCharacter(5)
+   }
+  };
+
+
+    //Function for generating a special character
+  function specialCharacter(length){
+    let result = '';
+    const specialChars = '!@#$%^&*'
+    const specialCharLength = specialChars.length;
+    for (var i = 0; i < length; i++){
+      result += specialChars.charAt(Math.floor(Math.random()* specialCharLength))
+    }
+    console.log(result)
+    password.push(result)
+    }
+
+
+    //Function for generating a random lowercase character
+  function specialCharacter(length){
+    let result = '';
+    const specialChars = 'abcdefghijklmnopqrstuvwxyz'
+    const specialCharLength = specialChars.length;
+    for (var i = 0; i < length; i++){
+      result += specialChars.charAt(Math.floor(Math.random()* specialCharLength))
+    }
+    console.log(result)
+    password.push(result)
+    }
+    
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
 
 
